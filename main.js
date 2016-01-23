3a07182d5327be7db90e00f60ddb908afb394960
ashuforweb <ashuforweb@gmail.com>
2016-01-23 04:22:04 -0800

    1.0

--- main.js
+++ main.js
@@ -1,28 +1,46 @@
 function fnRun() {
-  //setInterval(fnPrintDate,1000);
+  setInterval(fnPrintDate, 1000);
 }
 
-function markElem(element, index, array) {
-  if (element == "1") {
-    //document.getElementById('h'+index).className();
-    //document.getElementById('h'+index.toString()).className +=" mdl-button--colored";
-    console.log(document.getElementById('h'+index.toString()));
-  } else {
-    //  document.getElementById('h'+index.toString()).className = document.getElementById('h'+index.toString()).className.replace(" mdl-button--colored","");
-    console.log(document.getElementById('h'+index.toString()));
+function markElem(arr, e) {
+for(var j=0; j<arr.length;j++){
+      document.getElementById(e + j.toString()).className = document.getElementById(e + j.toString()).className.replace(" mdl-button--colored mdl-button--raised", "");
+}
+
+  for (var i = 0; i < arr.length; i++) {
+
+    if (arr[i] == "1") {
+      //document.getElementById('h'+index).className();
+      //console.log(hArray);
+      document.getElementById(e + i.toString()).className += " mdl-button--colored mdl-button--raised";
+      //console.log(document.getElementById('h'+index.toString()));
+    } else {
+      document.getElementById(e + i.toString()).className = document.getElementById(e + i.toString()).className.replace(" mdl-button--colored mdl-button--raised", "");
+      //console.log(document.getElementById('h'+index.toString()));
+    }
   }
-
 }
+
 
 function fnPrintDate() {
   var date = new Date();
   hArray = date.getHours().toString(2).split('');
   mArray = date.getMinutes().toString(2).split('');
   sArray = date.getSeconds().toString(2).split('');
-  console.log(sArray);
-  hArray.forEach(markElem);
-  mArray.forEach(markElem);
-  sArray.forEach(markElem);
-
-
+  var hfArray = new Array(5-hArray.length);
+  //var mhfArray = hfArray.fill(0);
+  var fhArray = hfArray.fill(0).concat(hArray);
+  var mfArray = new Array(6-mArray.length);
+  var fmArray = mfArray.fill(0).concat(mArray);
+  var sfArray = new Array(6-sArray.length);
+  var fsArray = sfArray.fill(0).concat(sArray);
+//  hArray.length=5;
+//  mArray.length=6;
+//  sArray.length=6;
+  //console.log(date.getHours()+":"+fhArray);
+  //  console.log(date.getMinutes()+":"+fmArray);
+//  console.log(date.getSeconds()+":"+fsArray);
+  markElem(fhArray.reverse(), 'h');
+  markElem(fmArray.reverse(), 'm');
+  markElem(fsArray.reverse(), 's');
 }
