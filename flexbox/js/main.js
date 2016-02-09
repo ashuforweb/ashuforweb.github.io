a54311b11b4205c474ab04bcc61cb15d9d78c0ad
ashuforweb <ashuforweb@gmail.com>
2016-02-09 00:08:24 -0800

    1.0

--- flexbox/js/main.js
+++ flexbox/js/main.js
@@ -11,19 +11,32 @@ function addToScore() {
   // console.log(scpush);
   // console.info("Score" + score);
   document.getElementById('fnscr').innerHTML = score;
-  console.log(scpush.slice(scpush.length - 6, scpush.length));
-  fnStream(scpush.slice(scpush.length - 6, scpush.length));
+  var scpushing = scpush;
+  var sclength = scpush.length;
+  if (sclength < 6) {
+    scpushing.length = 6;
+    scpushing.fill(0, sclength, 6);
+    fnStream(scpushing);
+  } else {
+    scpushing.reverse();
+    scpushing.length = 6;
+    scpushing.reverse();
+    fnStream(scpushing);
+  }
+  // console.log(scpush.slice(scpush.length - 6, scpush.length));
+
 }
 
 function fnStream(trm) {
   var strmdata = "";
-  for (var j = 6; j > 0; j--) {
+  for (var j = 0; j < trm.length; j++) {
     if (j % 2 == 0) {
-      strmdata += '<div class="ball">' + trm[j] + '</div>';
+      strmdata += '<div class="strmi ball">' + trm[j] + '</div>';
     } else {
-      strmdata += '<div class="ball balle">' + trm[j] + '</div>';
+      strmdata += '<div class="strmi ball balle">' + trm[j] + '</div>';
     }
   }
+console.log(strmdata);
   document.getElementById('strm').innerHTML = strmdata;
 }
 
