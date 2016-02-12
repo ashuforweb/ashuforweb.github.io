7ea229a4e7a6bdc9236ddc552d86d5e27f845b92
ashuforweb <ashuforweb@gmail.com>
2016-02-11 23:12:32 -0800

    push

--- angular/angular2-quickstart/app/app.component.js
+++ angular/angular2-quickstart/app/app.component.js
@@ -2,7 +2,7 @@
   app.AppComponent =
     ng.core.Component({
       selector: 'my-app',
-      template: '<h1>Yo! My First Angular 2 App! what\'s up</h1>'
+      template: '<h1>Yo! My First Angular 2 App! whats up</h1>'
     })
     .Class({
       constructor: function() {}
